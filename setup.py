386d2cc9709cab0a0b31ea7c53c2dd88035a1444
Emanuel Fontelles <emanuelfontelles@fisica.ufc.br>
2019-12-07 22:39:48 -0300

    added files

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@
 from setuptools import setup, find_packages
 
 
-with open('README.md') as f:
+
+with open('README.rst') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
@@ -16,6 +17,7 @@ setup(
     version='0.0.1',
     description='A package which contains a simple implementation of neural network with numpy',
     long_description=readme,
+    long_description_content_type='text/markdown',
     author='Emanuel Fontelles',
     author_email='emanuelfontelles@hotmail.com',
     url='https://github.com/EmanuelFontelles/nn_numpy',
